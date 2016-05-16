6819b97d0c90cd294c337fbd15492b51a2862138
razvan393 <razvanghinea05@gmail.com>
2016-05-16 21:58:01 +0300

    First commit

---
+++ public/search.php
@@ -0,0 +1,37 @@
+<?php
+//database configuration
+$dbHost = 'localhost';
+$dbUsername = 'root';
+$dbPassword = 'mysql';
+$dbName = 'vot2016';
+
+//connect with the database
+$db = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+if($db->connect_error) {
+    echo 'Database connection failed. Error' . $db->connect_errno . ' ' . $db->connect_error;
+    exit;
+} else {
+    $db->set_charset('utf8');
+}
+
+//get search term
+$searchTerm = trim(strip_tags($_GET['term']));
+$a_json = array();
+$a_json_row = array();
+//get matched data from table
+if ($query = $db->query("SELECT * FROM date WHERE localitate LIKE '%".$searchTerm."%' ORDER BY localitate ASC")){
+    while ($row = mysqli_fetch_array($query)) {
+        $location = htmlentities(stripcslashes($row['localitate']));
+        /*$judet = htmlentities(stripcslashes($row['judet']));*/
+        $a_json_row['location'] = $location;
+        /*$a_json_row['judet'] = $judet;*/
+        array_push($a_json, $a_json_row);
+    }
+}
+
+//return json data
+echo json_encode($a_json);
+flush();
+
+$db->close();
+?>
